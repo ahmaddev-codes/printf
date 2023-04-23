5829ef5b2f02a0d0233be4536ae9e0f7ea9922b8
iAhm-codes <omegabliss23@gmail.com>
2023-04-23 03:09:09 +0100

    Code: modified main.h header file

--- main.h
+++ main.h
@@ -15,17 +15,17 @@ typedef struct specifiers
 	int (*f)(va_list args);
 } spc_dt;
 
-char _write(char c);
+int _write(char c);
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_format(const char *format, va_list args);
-int print_spec(char format, va_list args);
-int print_invalid_spec(char prev_format, char format, int count);
-int print_integer(va_list args);
-void recursion_integer(int a);
-int print_int_binary(va_list args);
-void recursion_int_binary(int a);
-int validate_char(char type);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
 
 #endif /* MAIN_H */
