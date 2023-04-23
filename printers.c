376437d5e38470adcbaef5bcd8ef5a86d2988f33
iAhm-codes <omegabliss23@gmail.com>
2023-04-23 03:09:57 +0100

    Code: modified code in printers.c

--- printers.c
+++ printers.c
@@ -1,25 +1,26 @@
 #include "main.h"
-#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-  * print_char - Prints a char
+  * _print_a_char - Prints a char
   * @args: A list of variadic arguments
   *
   * Return: The length of the character
   */
-int print_char(va_list args)
+int _print_a_char(va_list args)
 {
 	_write(va_arg(args, int));
 	return (1);
 }
 
 /**
-  * print_string - Prints a string
+  * _print_a_string - Prints a string
   * @args: A list of variadic arguments
   *
   * Return: The length of the string
   */
-int print_string(va_list args)
+int _print_a_string(va_list args)
 {
 	char *arg = va_arg(args, char *);
 	int i = 0;
@@ -45,19 +46,18 @@ int print_string(va_list args)
 }
 
 /**
-  * print_integer - Prints a integer
+  * _print_a_integer - Prints a integer
   * @args: A list of variadic arguments
   *
   * Return: The length of the string
   */
-int print_integer(va_list args)
+int _print_a_integer(va_list args)
 {
 	int count = 1, m = 0;
 	unsigned int n = 0;
 
 	n = va_arg(args, int);
 	m = n;
-
 	if (m < 0)
 	{
 		_write('-');
@@ -71,22 +71,22 @@ int print_integer(va_list args)
 		count++;
 	}
 
-	recursion_integer(m);
+	_recursion_integer(m);
 	return (count);
 }
 
 /**
-  * recursion_integer - Prints a integer
+  * _recursion_integer - Prints a integer
   * @a: integer to print
   *
   * Return: Nothing
   */
-void recursion_integer(int a)
+void _recursion_integer(int a)
 {
 	unsigned int t;
 
 	t = a;
 	if (t / 10)
-		recursion_integer(t / 10);
+		_recursion_integer(t / 10);
 	_write(t % 10 + '0');
 }
